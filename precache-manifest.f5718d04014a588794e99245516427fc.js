3f7e90aed9ed3a7cebce21d68ef292a193a92934
Ashwin Kumar R P <rpaswin008@gmail.com>
2020-05-03 16:35:15 +0530

    Updates

---
+++ precache-manifest.f5718d04014a588794e99245516427fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8b71b77c7402a47565a6943b869bf9",
+    "url": "/Covid-19-Dashboard/index.html"
+  },
+  {
+    "revision": "52f86a4bdc49079b1c48",
+    "url": "/Covid-19-Dashboard/static/css/main.8395d8e5.chunk.css"
+  },
+  {
+    "revision": "94be29093d35097521f7",
+    "url": "/Covid-19-Dashboard/static/js/2.a223dc21.chunk.js"
+  },
+  {
+    "revision": "52f86a4bdc49079b1c48",
+    "url": "/Covid-19-Dashboard/static/js/main.a6def69b.chunk.js"
+  },
+  {
+    "revision": "4a24080eb44fa185f4ed",
+    "url": "/Covid-19-Dashboard/static/js/runtime-main.07676587.js"
+  },
+  {
+    "revision": "2e8a320b98659a1a3cecab4c79be35bd",
+    "url": "/Covid-19-Dashboard/static/media/logo.2e8a320b.png"
+  }
+]);
